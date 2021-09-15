1a1200a139d36f947e7e0e22a796dfa43c741cbf
chiha choi <chihachoi@outlook.com>
2021-09-15 20:27:23 +0100

    add initial unit tests

---
+++ src/tests/Graph.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import Graph from "../components/Graph";
+import Data from "./test-response-intraday-price";
+
+describe("Graph", () => {
+  test("renders Graph component", () => {
+    render(<Graph data={Data} />);
+    expect(screen.getByText("Meta Data")).toBeInTheDocument();
+  });
+  test("renders Graph component and loads test data", () => {
+    render(<Graph data={Data} />);
+    expect(screen.getByText("2. Symbol: GME")).toBeInTheDocument();
+    expect();
+  });
+});
